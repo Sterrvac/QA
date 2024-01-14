ca32d5c71b8e877e1db69f8d1a1814368ceeb8d8
Sterrvac <Slavok.slavok2000@yandex.ru>
2024-01-14 23:06:45 +0300

    new unittest and selenium

---
+++ selenium_course/test_abs_unittest_last_3.2.py
@@ -0,0 +1,12 @@
+import unittest
+from selenium import webdriver
+
+class TestPages(unittest.TestCase):
+    def page1():
+        link = "http://suninjuly.github.io/registration1.html"
+        
+        
+#Доделаю когда selenium будет нормально работать с новыми версиями Google Chrome
+        
+        
+    
